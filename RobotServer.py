bd6c9de8e8c2b44da119956db90042466f2e0448
yukitj <yukitj@163.com>
2019-08-29 15:12:53 +0800

    first commit

---
+++ RobotServer.py
@@ -0,0 +1,45 @@
+import socketserver, time, json
+
+rbts_state = {}
+state_req = [b'cart_pos\r', b'jnt_pos\r', b'jnt_vel\r', b'jnt_trq\r', b'alarm_info\r', b'state\r', b'robot_running_state\r']
+
+class RokaeRbtHandler(socketserver.BaseRequestHandler):
+
+    def handle(self):
+        key = self.client_address[0]
+        l = len(state_req)
+        r = [None] * l
+        conn = True
+        if key == '192.168.2.100':
+            a = self.request.recv(1024)
+            if a == b'get_robots_info':
+                self.request.sendall(json.dumps(rbts_state).encode('utf-8'))
+            else:
+                self.request.sendall(b'{}')
+            return
+        while conn:
+            time.sleep(0.1)
+            for i in range(l):
+                try:
+                    self.request.sendall(state_req[i])
+                    a = self.request.recv(1024)
+                    #print(a)
+                except ConnectionError:
+                    conn = False
+                    break
+                if not a:
+                    conn = False
+                    break
+                r[i] = a.strip().decode('utf-8')
+            if conn:
+                rbts_state[key] = r
+            else:
+                rbts_state.pop(key)
+
+
+if __name__ == "__main__":
+
+    HOST, PORT = "192.168.2.100", 9999
+
+    with socketserver.ThreadingTCPServer((HOST, PORT), RokaeRbtHandler) as server:
+        server.serve_forever()
